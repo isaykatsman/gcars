0a24f11e35aa101cb6d2585259fc0a1fa958c427
John Macdonald <jm2424@cornell.edu>
2016-11-12 15:18:10 -0500

    genetic: don't overload functions, don't wrap in extraneous module

--- interfaces/geneticalgorithm.mli
+++ interfaces/geneticalgorithm.mli
@@ -1,25 +1,28 @@
-(* A [GenAlgorithm] is a module that has genetic algorithm
- * subroutines to generate a new population from an old one 
- * given the results of each car in the old population. It 
- * also has a subroutine for randomly generating a generation of cars
- * from scratch. *)
-module type GenAlgorithm = sig
-  (* type for a car (genome) 
-   * genome has 8 genes for vertices of car body, 1 gene for front wheel radius,
-   * and 1 other gene for back wheel radius*)
-  type car = {v1:float*float;v2:float*float;v3:float*float;v4:float*float;
-              v5:float*float;v6:float*float;v7:float*float;v8:float*float;
-              r1:float;r2:float}
+(* type for a car (genome) 
+ * genome has 8 genes for vertices of car body, 1 gene for front wheel radius,
+ * and 1 other gene for back wheel radius *)
+type car = {
+  v1: float * float;
+  v2: float * float;
+  v3: float * float;
+  v4: float * float;
+  v5: float * float;
+  v6: float * float;
+  v7: float * float;
+  v8: float * float;
+  r1: float;
+  r2: float;
+}
+ 
+(* An existing population is represented by a Population type; an empty one
+ * is represented by Empty n where n is the size of the desired population *)
+type population = 
+  | Population of car list
+  | Empty of int
 
-  (* population type is simply a list of cars *)
-  type population = car list
-
-  (* randomly generated initial population given the number of cars
-   * in the population *)
-  val initpopulation : int -> population
-
-  (* breed a new population from a previous one given the results 
-   * of the previous population *)
-  val initpopulation : population -> results -> population
-
-end
+(* [new_population p r] is a new population, calculated via a genertic
+ * algorithm, given the results [r] of the previous population and the
+ * and old population [p] itself. If population is Empty n, then an inital
+ * population of size n *)
+val new_population : (pop : population) -> (results : float list) -> 
+  (float : mutation_rate) -> (new_pop : population)
