b35920a649296276530a613916d34fabaff83602
John Macdonald <jm2424@cornell.edu>
2016-11-23 14:12:17 -0500

    genetic: Add FakeGenetic implementation

--- genetic.ml
+++ genetic.ml
@@ -1,3 +1,9 @@
-module Genetic = struct
-
+module type GeneticCarAlgo = sig
+  val new_population : population -> scores -> float -> population
 end
+
+module FakeGenetic = struct
+  let new_population pop scores rate = pop
+end
+
+module Genetic = FakeGenetic
