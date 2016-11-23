f1b50c6b0b4629c8d60b7693d17ecc24150e0c9c
John Macdonald <jm2424@cornell.edu>
2016-11-22 20:09:45 -0500

    world: wrap in module and use Vect to represent car position

--- world.mli
+++ world.mli
@@ -1,25 +1,28 @@
 open Genetic
-open Chipmunk
-open OO
+open Vect
 
-(* Abstract type to represent the world *)
-type world
+module World = sig
+  (* Abstract type to represent the world *)
+  type world
 
-type car_info = {
-  velocity : float;
-  x : float;
-  y : float;
-  rot : float;
-} 
+  type car_state = {
+    velocity : float;
+    pos : Vect.t;
+    angle : float; (* in radians *)
+  } 
 
-(* Creates a new world with the specified terrain and cars *)
-val create_world : population -> world
+  (* Creates a new world with the specified terrain and cars *)
+  val create_world : population -> world
 
-(* Steps the physics simulation forward one timestep *)
-val step : world -> world
+  (* Steps the physics simulation forward one timestep *)
+  val step : world -> world
 
-(* Get info about all the cars *)
-val get_car_info : world -> car_info list
+  (* Get state info about all the cars *)
+  val get_car_state : world -> car_state list
 
-(* Get the terrain to be drawn *)
-val get_terrain : world -> (float * float) list
+  (* Get the terrain to be drawn *)
+  val get_terrain : world -> Vect.t list
+end
+
+module World : World
+module FakeWorld : World
