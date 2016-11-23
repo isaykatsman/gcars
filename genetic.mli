1159dea68e1e7a6dcbdc198b4956ac9198fb5e2c
John Macdonald <jm2424@cornell.edu>
2016-11-23 15:14:11 -0500

    genetic: move population and car types outside of Genetic module

--- genetic.mli
+++ genetic.mli
@@ -1,26 +1,26 @@
+(* type for a car (genome) 
+ * genome has 8 genes for vertices of car chassis, and for each of the two wheels,
+ * one gene for radius and one for the number of the vertex it will be attached
+ * to. The position of each vertex is expressed in polar coordinates, with
+ * the second float in the pair being the angle and the first being the
+ * radius *)
+type car_genome = {
+  chassis : (float * float) list;
+  w1_radius: float;
+  w2_radius: float;
+  w1_vert: int;
+  w2_vert: int;
+}
+ 
+(* An existing population is represented by a Population type; an empty one
+ * is represented by Empty n where n is the size of the desired population *)
+type population = 
+  | Population of car_genome list
+  | Empty of int
+
+type scores = float list
+
 module type GeneticCarAlgo = sig
-  (* type for a car (genome) 
-   * genome has 8 genes for vertices of car chassis, and for each of the two wheels,
-   * one gene for radius and one for the number of the vertex it will be attached
-   * to. The position of each vertex is expressed in polar coordinates, with
-   * the second float in the pair being the angle and the first being the
-   * radius *)
-  type car_genome = {
-    chassis : (float * float) list;
-    w1_radius: float;
-    w2_radius: float;
-    w1_vert: int;
-    w2_vert: int;
-  }
-   
-  (* An existing population is represented by a Population type; an empty one
-   * is represented by Empty n where n is the size of the desired population *)
-  type population = 
-    | Population of car_genome list
-    | Empty of int
-
-  type scores = float list
-
   (* [new_population pop scores rate] is a new population, calculated via a genertic
    * algorithm, given the scores [scores] of the previous population and the
    * and old population [pop] itself. If population is Empty n, then an inital
@@ -29,3 +29,4 @@ module type GeneticCarAlgo = sig
 end
 
 module Genetic : GeneticCarAlgo
+module FakeGenetic : GeneticCarAlgo
