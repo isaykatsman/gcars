e1820c83de4a4fb334499d2a50ad00d3db88eb31
John Macdonald <jm2424@cornell.edu>
2016-11-23 21:47:15 -0500

    genetic: fix errors preventing compilation

--- genetic.ml
+++ genetic.ml
@@ -1,3 +1,17 @@
+type car_genome = {
+  chassis : (float * float) list;
+  w1_radius: float;
+  w2_radius: float;
+  w1_vert: int;
+  w2_vert: int;
+}
+ 
+type population = 
+  | Population of car_genome list
+  | Empty of int
+
+type scores = float list
+
 module type GeneticCarAlgo = sig
   val new_population : population -> scores -> float -> population
 end
