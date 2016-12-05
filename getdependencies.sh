7e0d2fe51265ff6d3b0b844f5b6a0b0cc8fb644d
Ubuntu <ubuntu@csvm>
2016-12-05 04:16:51 -0500

    Added script and instructions for a new user to obtain all necessary dependencies.

---
+++ getdependencies.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get install build-essential
+sudo apt-get install libjpeg-dev
+sudo apt-get install freeglut3-dev
+sudo apt-get install chipmunk-dev
+cd chipmunk && make
+cd glMLite && make
+
+
